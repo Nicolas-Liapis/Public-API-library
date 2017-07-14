cc67d40900025b9c94e9c1d755e05d74780aaf5b
Nicolas <nicolas.liapis.dev@gmail.com>
2017-07-14 13:01:52 +0100

    v0.2
    lightbox working

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  var cardsAPI = 'https://deckofcardsapi.com/api/deck/new/draw/?count=6';
+
+  function displayCards(data) {
+    var cardsHTML = '<ul class="deck">';
+    $.each(data.cards, function (i, card) {
+      cardsHTML += '<li class="card">';
+      cardsHTML += '<a href=" ' + card.image + '" rel="lightbox" target="_blank" data-title="Value: ' + card.value + ' </br> Suit: ' + card.suit + ' </br> Code: ' + card.code + '"><img src=" ' + card.image + ' " >';
+      cardsHTML += '</a></li>';
+    });
+    cardsHTML += '</ul>';
+    $('#cards').html(cardsHTML);
+  }
+  $.getJSON(cardsAPI, displayCards);
+
+});
+
+// lightbox.option({
+//   'fitImagesInViewport': true,
+//   'showImageNumberLabel': false,
+//   'wrapAround': true,
+//   'albumLabel': "test"
+// });
